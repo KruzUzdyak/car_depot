27d0219c3095430b923747c50d1dc8365e824bb3
Kruz <kruzuzdyak.96@gmail.com>
2022-01-21 13:57:05 +0300

    Complete AdminDaoIT

--- src/test/java/com/epam/volodko/dao/AdminDaoIT.java
+++ src/test/java/com/epam/volodko/dao/AdminDaoIT.java
@@ -9,20 +9,41 @@ import com.epam.volodko.entity.user.RoleProvider;
 import org.flywaydb.core.internal.jdbc.RowMapper;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.experimental.theories.DataPoints;
+import org.junit.experimental.theories.Theories;
+import org.junit.experimental.theories.Theory;
+import org.junit.runner.RunWith;
 
 import java.io.IOException;
 import java.sql.SQLException;
 import java.util.Date;
+import java.util.List;
 
 import static org.junit.Assert.assertEquals;
 
-public class AdminDaoIT extends DataBaseIT{
+@RunWith(Theories.class)
+public class AdminDaoIT extends DataBaseIT {
 
     private static final String FIND_ADMIN_BY_ID_QUERY = String.format(
             "SELECT * FROM %s AS u JOIN %s AS r ON u.%s = r.%s LEFT JOIN %s ai ON u.%s = ai.%s WHERE u.%s = ?;",
             Table.USERS, Table.ROLES, Column.USERS_ROLE_ID, Column.ROLES_ID, Table.ADMIN_INFO,
             Column.USERS_ID, Column.ADMIN_INFO_USER_ID, Column.USERS_ID);
     private final DAOFactory factory = DAOFactory.getInstance();
+    private static final String FIND_ADMIN_BY_LOGIN_QUERY = String.format(
+            "SELECT * FROM %s AS u JOIN %s AS r ON u.%s = r.%s LEFT JOIN %s ai ON u.%s = ai.%s WHERE u.%s = ?;",
+            Table.USERS, Table.ROLES, Column.USERS_ROLE_ID, Column.ROLES_ID, Table.ADMIN_INFO,
+            Column.USERS_ID, Column.ADMIN_INFO_USER_ID, Column.USERS_LOGIN);
+    private static final String FIND_ALL_ADMINS_QUERY = String.format(
+            "SELECT * FROM %s AS u JOIN %s AS r ON u.%s = r.%s LEFT JOIN %s ai ON u.%s = ai.%s WHERE r.%s = ?;",
+            Table.USERS, Table.ROLES, Column.USERS_ROLE_ID, Column.ROLES_ID, Table.ADMIN_INFO,
+            Column.USERS_ID, Column.ADMIN_INFO_USER_ID, Column.ROLES_ID);
+
+    @DataPoints
+    public static int[] adminIds = new int[]{1, 2};
+    @DataPoints
+    public static String[] logins = new String[]{"admin1", "admin2"};
+
+    private final UserDAO<Admin> adminDAO = DAOFactory.getInstance().getAdminDAO();
 
     @Before
     public void init() throws IOException, SQLException {
@@ -30,26 +51,72 @@ public class AdminDaoIT extends DataBaseIT{
                 Query.SQL_FILL_ROLES, Query.SQL_FILL_USERS, Query.SQL_FILL_ADMIN_INFO);
     }
 
-    @Test
-    public void testFindAdminById() throws SQLException, DAOException {
-        int userId = 1;
+    @Theory
+    public void testFindById(int adminId) throws SQLException, DAOException {
         Admin expectedAdmin = getJdbcTemplate()
-                .query(FIND_ADMIN_BY_ID_QUERY, adminMapper(), userId)
+                .query(FIND_ADMIN_BY_ID_QUERY, adminMapper(), adminId)
                 .get(0);
-        Admin actualAdmin = factory.getAdminDAO().findById(userId);
+        Admin actualAdmin = adminDAO.findById(adminId);
 
         assertEquals(expectedAdmin, actualAdmin);
 
-        userId = 2;
-        expectedAdmin = getJdbcTemplate()
-                .query(FIND_ADMIN_BY_ID_QUERY, adminMapper(), userId)
+    }
+
+    @Theory
+    public void testFindByLogin(String login) throws DAOException, SQLException {
+        Admin expectedAdmin = getJdbcTemplate()
+                .query(FIND_ADMIN_BY_LOGIN_QUERY, adminMapper(), login)
                 .get(0);
-        actualAdmin = factory.getAdminDAO().findById(userId);
+        Admin actualAdmin = adminDAO.findByLogin(login);
 
         assertEquals(expectedAdmin, actualAdmin);
     }
 
-    private RowMapper<Admin> adminMapper(){
+    @Test
+    public void testFindAll() throws DAOException, SQLException {
+        List<Admin> expectedAdmins = getJdbcTemplate()
+                .query(FIND_ALL_ADMINS_QUERY, adminMapper(), Role.ADMIN.getRoleId());
+        List<Admin> actualAdmins = adminDAO.findAll();
+
+        assertEquals(expectedAdmins, actualAdmins);
+    }
+
+    @Test
+    public void testSaveInfo() throws DAOException, SQLException {
+        Admin admin = new Admin(0, "stub", "stub", "stub",
+                "stub", Role.ADMIN, null, null);
+        adminDAO.saveNew(admin);
+        admin.setPassword(null);
+        int rowsAffected = adminDAO.saveInfo(admin.getId());
+        Admin actualAdmin = getJdbcTemplate()
+                .query(FIND_ADMIN_BY_ID_QUERY, adminMapper(), admin.getId())
+                .get(0);
+
+        assertEquals(1, rowsAffected);
+        assertEquals(admin, actualAdmin);
+    }
+
+    @Test
+    public void testUpdateInfo()throws DAOException, SQLException{
+        Admin admin = getJdbcTemplate()
+                .query(FIND_ADMIN_BY_ID_QUERY, adminMapper(), 1)
+                .get(0);
+        admin.setNote("new note");
+        admin.setWorksSince(new Date(10101010101010L));
+        Admin expectedAdmin = new Admin(admin.getId(), admin.getLogin(), admin.getPassword(), admin.getName(),
+                admin.getPhone(), admin.getRole(), admin.getWorksSince(), admin.getNote());
+        admin.setLogin("stub");
+        admin.setPhone("stub");
+        int rowsAffected = adminDAO.updateInfo(admin);
+        Admin actualAdmin = getJdbcTemplate()
+                .query(FIND_ADMIN_BY_ID_QUERY, adminMapper(), expectedAdmin.getId())
+                .get(0);
+
+        assertEquals(1, rowsAffected);
+        assertEquals(expectedAdmin, actualAdmin);
+    }
+
+    private RowMapper<Admin> adminMapper () {
         return (rs) -> {
             int id = rs.getInt(Column.USERS_ID);
             String login = rs.getString(Column.USERS_LOGIN);
@@ -59,7 +126,7 @@ public class AdminDaoIT extends DataBaseIT{
             String note = rs.getString(Column.ADMIN_INFO_NOTE);
             long workSince = rs.getLong(Column.ADMIN_INFO_WORKS_SINCE);
             Date date = null;
-            if (workSince > 0){
+            if (workSince > 0) {
                 date = new Date(workSince);
             }
             return new Admin(id, login, null, name, phone, role, date, note);
--- src/test/java/com/epam/volodko/dao/ClientDaoIT.java
+++ src/test/java/com/epam/volodko/dao/ClientDaoIT.java
@@ -3,26 +3,45 @@ package com.epam.volodko.dao;
 import com.epam.volodko.dao.exception.DAOException;
 import com.epam.volodko.dao.table_name.Column;
 import com.epam.volodko.dao.table_name.Table;
-import com.epam.volodko.entity.user.*;
+import com.epam.volodko.entity.user.Client;
+import com.epam.volodko.entity.user.Role;
+import com.epam.volodko.entity.user.RoleProvider;
 import org.flywaydb.core.internal.jdbc.RowMapper;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.experimental.theories.DataPoints;
+import org.junit.experimental.theories.Theories;
+import org.junit.experimental.theories.Theory;
+import org.junit.runner.RunWith;
 
 import java.io.IOException;
-import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.Date;
+import java.util.List;
 
 import static org.junit.Assert.assertEquals;
 
+@RunWith(Theories.class)
 public class ClientDaoIT extends DataBaseIT{
 
     private static final String FIND_CLIENT_BY_ID_QUERY = String.format(
             "SELECT * FROM %s AS u JOIN %s AS r ON u.%s = r.%s LEFT JOIN %s ci ON u.%s = ci.%s WHERE u.%s = ?;",
             Table.USERS, Table.ROLES, Column.USERS_ROLE_ID, Column.ROLES_ID, Table.CLIENT_INFO,
             Column.USERS_ID, Column.CLIENT_INFO_USER_ID, Column.USERS_ID);
+    private static final String FIND_CLIENT_BY_LOGIN_QUERY = String.format(
+            "SELECT * FROM %s AS u JOIN %s AS r ON u.%s = r.%s LEFT JOIN %s ci ON u.%s = ci.%s WHERE u.%s = ?;",
+            Table.USERS, Table.ROLES, Column.USERS_ROLE_ID, Column.ROLES_ID, Table.CLIENT_INFO,
+            Column.USERS_ID, Column.CLIENT_INFO_USER_ID, Column.USERS_LOGIN);
+    private static final String FIND_ALL_CLIENTS_QUERY = String.format(
+            "SELECT * FROM %s AS u JOIN %s AS r ON u.%s = r.%s LEFT JOIN %s ci ON u.%s = ci.%s WHERE r.%s = ?;",
+            Table.USERS, Table.ROLES, Column.USERS_ROLE_ID, Column.ROLES_ID, Table.CLIENT_INFO,
+            Column.USERS_ID, Column.CLIENT_INFO_USER_ID, Column.ROLES_ID);
 
-    private final DAOFactory factory = DAOFactory.getInstance();
+    @DataPoints
+    public static int[] ids = new int[]{3, 4};
+    @DataPoints
+    public static String[] logins = new String[]{"client1", "client2"};
+
+    private final UserDAO<Client> clientDAO = DAOFactory.getInstance().getClientDAO();
 
     @Before
     public void init() throws IOException, SQLException {
@@ -30,24 +49,37 @@ public class ClientDaoIT extends DataBaseIT{
                 Query.SQL_FILL_ROLES, Query.SQL_FILL_USERS, Query.SQL_FILL_CLIENT_INFO);
     }
 
-    @Test
-    public void testFindClientById() throws DAOException, SQLException {
-        int userId = 3;
+    @Theory
+    public void testFindById(int id) throws DAOException, SQLException {
         Client expectedClient = getJdbcTemplate()
-                .query(FIND_CLIENT_BY_ID_QUERY, clientMapper(), userId)
+                .query(FIND_CLIENT_BY_ID_QUERY, clientMapper(), id)
                 .get(0);
-        Client actualClient = factory.getClientDAO().findById(userId);
-
-        assertEquals(expectedClient, actualClient);
-
-        userId = 4;
-        expectedClient = getJdbcTemplate()
-                .query(FIND_CLIENT_BY_ID_QUERY, clientMapper(), userId)
-                .get(0);
-        actualClient = factory.getClientDAO().findById(userId);
+        Client actualClient = clientDAO.findById(id);
 
         assertEquals(expectedClient, actualClient);
     }
+
+    @Theory
+    public void testFindByLogin(String login) throws DAOException, SQLException{
+        Client expectedClient = getJdbcTemplate()
+                .query(FIND_CLIENT_BY_LOGIN_QUERY, clientMapper(), login)
+                .get(0);
+        Client actualClient = clientDAO.findByLogin(login);
+
+        assertEquals(expectedClient, actualClient);
+    }
+
+    @Test
+    public void testFindALl() throws DAOException, SQLException{
+        List<Client> expectedClients = getJdbcTemplate()
+                .query(FIND_ALL_CLIENTS_QUERY, clientMapper(), Role.CLIENT.getRoleId());
+        List<Client> actualClients = clientDAO.findAll();
+
+        assertEquals(expectedClients, actualClients);
+    }
+
+
+
 
     private RowMapper<Client> clientMapper(){
         return (rs) -> {
